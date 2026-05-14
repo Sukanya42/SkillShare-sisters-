728616b2a55c2fbb1a40175b01c6348d3399cedd
Sukanya42 <sukanyag2509@gmail.com>
2026-05-14 22:19:17 +0530

    Add files via upload

---
+++ server/Skill.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const SkillSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  category: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Skill", SkillSchema);
